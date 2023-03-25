ac5fb7e88126728aa2647f69c2b6845e2bc5718d
ridnex <ridnex28@gmail.com>
2023-03-25 12:48:59 +0600

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main(){
-    cout << 91/84;
+    cout << ceil(-2.6)<<endl;
+    cout << ceil(-2.1)<<endl;
+    cout << ceil(-0.04)<<endl;
+    cout << floor(-2.6)<<endl;
+    cout << floor(-2.1)<<endl;
+    cout << floor(-0.0004)<<endl;
 }
