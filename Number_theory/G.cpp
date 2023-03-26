164aae711fe8c63e0570b014b0f96e3003f984d2
ridnex <ridnex28@gmail.com>
2023-03-26 20:04:31 +0600

    up-solving G

--- Number_theory/G.cpp
+++ Number_theory/G.cpp
@@ -1,48 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
-// int log_prime(long long m){
-//     long long b;
-//     b = floor(sqrt(m));
-//     long long cnt = 0;
-//     vector <long long> u(b+1, 0);
-//     for(long long i=2; i<b+1; i++){
-//         if(u[i]==0){
-//             for(long long j=(i)*(i); j<b+1;j+=(i) ){
-//                 u[j]=1;   
-//             }
-//             cnt += floor(log(m)/log(i))-1;
+vector<bool>arr(1000005, true);
+vector <unsigned long long> barr;
 
-            
-//         }   
-//     }
-//     return cnt;
-
-// }
-int main(){
-    // int k;
-    // cin >> k;
-    cout << "lol"<<endl;
-    char arr[10000002];
-    fill(arr,arr+10000000,'0');
-    vector<long long> barr;
-    for(long long i=2; i<10000001; i++){
-        if(arr[i]=='0'){
-            for(long long j=(i)*(i); j<10000001;j+=(i) ){
-                arr[j]='1';   
+void primes(){
+    for (unsigned long long i = 2; i < 1000004; i++) {
+        if(arr[i]){
+            for(unsigned long long j = i * i; j < 1000004; j += i) {
+                arr[j] = false; 
             }
-            barr.push_back(i);
-        }   
+            unsigned long long k = i * i;
+            while(k<=1000000000005){
+                barr.push_back(k);
+                k*=i;
+            }
+            barr.push_back(k);
+        }
     }
-    for(long long i=0; i<barr.size(); i++){
-        cout << barr[i]<<" ";
-    }
+}
 
-    // for(int l=0; l<k;l++){
-    //     long long a, m;
-    //     cin >> a >> m;
-    //     cout<<log_prime(m)-log_prime(a-1)<<endl;
-    // }
+
+unsigned long long solving_low_hight(unsigned long long a, unsigned long long b){
     
+    unsigned long long cnt2 = lower_bound(barr.begin(), barr.end(), a) - barr.begin();
+    unsigned long long cnt1 = lower_bound(barr.begin(), barr.end(), b) - barr.begin();
+    if(cnt2==cnt1){
+        if(*lower_bound(barr.begin(), barr.end(), a) <= b && *lower_bound(barr.begin(), barr.end(), a) >= a){
+            return 1;
+        }
+        return 0;
+    }
+    return cnt1-cnt2;
+
+}
+
+
+int main(){
+    primes();
+    sort(barr.begin(), barr.end());
+    int n; 
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        unsigned long long a, b;
+        cin >> a >> b;
+        cout << solving_low_hight(a, b)<<endl;
+    }
 }
